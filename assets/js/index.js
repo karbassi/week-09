b14e8efacb63a666dffed865c8293faae2649b54
Ali Karbassi <ali@karbassi.com>
2020-03-31 12:10:46 -0500

    Bouncing off walls.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,15 @@
 const canvas = document.querySelector('#drawing');
 const ctx = canvas.getContext('2d');
 
-const obama = new Obama(100, 100);
+const obama = new Obama(250, 250, -25.2, 10.4);
+
+const tick = () => {
+  //   console.log('Tick');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  obama.tick();
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
--- assets/js/obama.js
+++ assets/js/obama.js
@@ -1,32 +1,68 @@
 class Obama {
-  image;
-  x;
-  y;
+  _image;
+  _x;
+  _y;
+  _speedX = 1;
+  _speedY = 1;
 
-  constructor(theX, theY) {
-    console.log("We've loaded Obama!");
-    console.log(theX, theY);
+  constructor(x, y, speedX, speedY) {
+    // console.log("We've loaded Obama!");
+    // console.log(x, y);
 
-    this.x = theX;
-    this.y = theY;
+    this._x = x;
+    this._y = y;
+
+    if (speedX !== undefined) {
+      this._speedX = speedX;
+    }
+
+    if (speedY !== undefined) {
+      this._speedY = speedY;
+    }
 
     this.loadImages();
   }
 
   loadImages() {
-    this.image = new Image();
+    this._image = new Image();
     // this.image.onload = function() {
     //   this.draw();
     // };
-    this.image.onload = () => {
+    this._image.onload = () => {
       this.draw();
     };
-    this.image.src = './assets/img/obama.png';
+    this._image.src = './assets/img/obama.png';
   }
 
   draw() {
     ctx.beginPath();
     //  drawImage(image, dx, dy, dWidth, dHeight);
-    ctx.drawImage(this.image, 100, 100, 100, 100);
+    ctx.drawImage(this._image, this._x, this._y, 100, 100);
+  }
+
+  move() {
+    if (this._y <= 0) {
+      this._speedY *= -1;
+    }
+
+    if (this._y + 100 >= canvas.height) {
+      this._speedY *= -1;
+    }
+
+    if (this._x <= 0) {
+      this._speedX *= -1;
+    }
+
+    if (this._x + 100 >= canvas.width) {
+      this._speedX *= -1;
+    }
+
+    this._x += this._speedX;
+    this._y += this._speedY;
+  }
+
+  tick() {
+    this.move();
+    this.draw();
   }
 }
